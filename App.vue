1d1df6e0651201030f943045ad3bae75a9f0f791
Mr.cat <fantasy.lian@163.com>
2020-06-08 16:35:57 +0800

    modify title

--- App.vue
+++ App.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="app">
+        <h1>省、市、区县、街道四级联动</h1>
         <distpicker v-model="defaultAddress"></distpicker>
     </div>
 </template>
 <script>
-import Distpicker from './components/Distpicker';
+import Distpicker from './components/Distpicker.vue';
 export default {
+    name: 'app',
     components: { Distpicker },
     data() {
         return {
